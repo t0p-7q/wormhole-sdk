83b6aa687053b9fb3edd8ebae94a191aa8a3f180
Néstor Coppi <nestorcoppi@gmail.com>
2023-12-07 12:31:46 -0300

    Init

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import TESTNET_CONFIG from './config/TESTNET';
+import MAINNET_CONFIG from './config/MAINNET';
+import DEVNET_CONFIG from './config/DEVNET';
+
+export type {
+  TestnetChainId,
+  TestnetChainName,
+  ChainContracts as TestnetChainContracts,
+} from './config/TESTNET';
+
+export * from './types';
+export * from './vaa';
+export * from './config/MAINNET';
+export * from './wormhole';
+export * from './contexts/solana';
+export * from './contexts/eth';
+export * from './contexts/sui';
+export * from './contexts/aptos';
+export * from './contexts/sei';
+export * from './contexts/cosmos';
+export * from './abis';
+export * from './utils';
+
+export const CONFIG = {
+  MAINNET: MAINNET_CONFIG,
+  TESTNET: TESTNET_CONFIG,
+  DEVNET: DEVNET_CONFIG,
+};
